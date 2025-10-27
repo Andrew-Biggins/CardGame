584902f515108be57a200d22c55d978c3f324ce1
Andrew Biggins <andrewbiggins@live.co.uk>
2025-10-27 11:05:38 +0000

    Add parsing for single card

---
+++ CardGame.Engine/Model/Card.cs
@@ -0,0 +1,31 @@
+﻿namespace CardGame.Engine.Model;
+
+internal readonly record struct Card(CardRank Value, CardSuit Suit);
+
+public enum CardSuit
+{
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+    None 
+}
+
+public enum CardRank
+{
+    None = 0,
+    Joker = 0, // Not used for ranking
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Ace = 14,
+}
---
+++ CardGame.Engine/Services/CsvCardParser.cs
@@ -0,0 +1,67 @@
+﻿using CardGame.Engine.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Engine.Services;
+
+internal sealed class CsvCardParser
+{
+    public static bool TryParseCard(string input, out Card card)
+    {
+        card = new Card(CardRank.None, CardSuit.None); // Placeholder for unrecognised card
+
+        if (string.IsNullOrWhiteSpace(input) || input.Length < 2)
+            return false;
+
+        input = input.Trim().ToUpperInvariant();
+
+        if (input.Length != 2) return false;
+
+        if (input == "JR")
+        {
+            card = new Card(CardRank.Joker, CardSuit.None);
+            return true;
+        }
+
+        var rankCode = input[0];
+        var suitCode = input[1];
+
+        var rank = rankCode switch
+        {
+            '2' => CardRank.Two,
+            '3' => CardRank.Three,
+            '4' => CardRank.Four,
+            '5' => CardRank.Five,
+            '6' => CardRank.Six,
+            '7' => CardRank.Seven,
+            '8' => CardRank.Eight,
+            '9' => CardRank.Nine,
+            'T' => CardRank.Ten,
+            'J' => CardRank.Jack,
+            'Q' => CardRank.Queen,
+            'K' => CardRank.King,
+            'A' => CardRank.Ace,
+            _ => CardRank.None 
+        };
+
+        if (rank == CardRank.None) return false;
+
+        var suit = suitCode switch
+        {
+            'H' => CardSuit.Hearts,
+            'D' => CardSuit.Diamonds,
+            'C' => CardSuit.Clubs,
+            'S' => CardSuit.Spades,
+            _ => CardSuit.None
+        };
+
+        if (suit == CardSuit.None) return false;
+
+        card = new Card(rank, suit);
+
+        return true;
+    }
+}
